b9057f39a37f621c69d7ca3517d998ba166a795b
Julien Robert <julien@rbrt.fr>
2021-04-23 15:42:50 +0200

    Increase pricing

--- components/landing/pricing.vue
+++ components/landing/pricing.vue
@@ -33,7 +33,7 @@
                   </h3>
                 </div>
                 <div class="mt-4 flex items-baseline text-6xl font-extrabold">
-                  €15
+                  €20
                   <span class="ml-1 text-2xl font-medium text-gray-500">
                     eenmalig
                   </span>
