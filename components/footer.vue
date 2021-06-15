73f6662cbbad786ea8da8124975b9772ce7f14cc
Julien Robert <julien@rbrt.fr>
2021-06-15 14:08:25 +0200

    Update social network

--- components/footer.vue
+++ components/footer.vue
@@ -5,7 +5,13 @@
         <div class="md:grid md:grid-cols-2 md:gap-8">
           <div>
             <h3
-              class="text-sm font-semibold text-gray-400 tracking-wider uppercase"
+              class="
+                text-sm
+                font-semibold
+                text-gray-400
+                tracking-wider
+                uppercase
+              "
             >
               Ontdek
             </h3>
@@ -38,7 +44,13 @@
           </div>
           <div class="mt-12 md:mt-0">
             <h3
-              class="text-sm font-semibold text-gray-400 tracking-wider uppercase"
+              class="
+                text-sm
+                font-semibold
+                text-gray-400
+                tracking-wider
+                uppercase
+              "
             >
               WoningFinder
             </h3>
@@ -76,7 +88,13 @@
         <div class="md:grid md:grid-cols-2 md:gap-8">
           <div>
             <h3
-              class="text-sm font-semibold text-gray-400 tracking-wider uppercase"
+              class="
+                text-sm
+                font-semibold
+                text-gray-400
+                tracking-wider
+                uppercase
+              "
             >
               Voorwaarden
             </h3>
@@ -112,7 +130,14 @@
         </div>
       </div>
       <div
-        class="mt-8 border-t border-gray-700 pt-8 md:flex md:items-center md:justify-between"
+        class="
+          mt-8
+          border-t border-gray-700
+          pt-8
+          md:flex
+          md:items-center
+          md:justify-between
+        "
       >
         <div class="flex space-x-6 md:order-2">
           <a
@@ -136,12 +161,12 @@
           </a>
 
           <a
-            href="https://fb.me/woningfinder"
+            href="https://instagram.com/woningfinder"
             target="_blank"
             rel="noopener noreferrer"
             class="text-gray-400 hover:text-gray-300"
           >
-            <span class="sr-only">Facebook</span>
+            <span class="sr-only">Instagram</span>
 
             <svg
               class="h-6 w-6"
@@ -151,7 +176,7 @@
             >
               <path
                 fill-rule="evenodd"
-                d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-3 7h-1.924c-.615 0-1.076.252-1.076.889v1.111h3l-.238 3h-2.762v8h-3v-8h-2v-3h2v-1.923c0-2.022 1.064-3.077 3.461-3.077h2.539v3z"
+                d="M15.233 5.488c-.843-.038-1.097-.046-3.233-.046s-2.389.008-3.232.046c-2.17.099-3.181 1.127-3.279 3.279-.039.844-.048 1.097-.048 3.233s.009 2.389.047 3.233c.099 2.148 1.106 3.18 3.279 3.279.843.038 1.097.047 3.233.047 2.137 0 2.39-.008 3.233-.046 2.17-.099 3.18-1.129 3.279-3.279.038-.844.046-1.097.046-3.233s-.008-2.389-.046-3.232c-.099-2.153-1.111-3.182-3.279-3.281zm-3.233 10.62c-2.269 0-4.108-1.839-4.108-4.108 0-2.269 1.84-4.108 4.108-4.108s4.108 1.839 4.108 4.108c0 2.269-1.839 4.108-4.108 4.108zm4.271-7.418c-.53 0-.96-.43-.96-.96s.43-.96.96-.96.96.43.96.96-.43.96-.96.96zm-1.604 3.31c0 1.473-1.194 2.667-2.667 2.667s-2.667-1.194-2.667-2.667c0-1.473 1.194-2.667 2.667-2.667s2.667 1.194 2.667 2.667zm4.333-12h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm.952 15.298c-.132 2.909-1.751 4.521-4.653 4.654-.854.039-1.126.048-3.299.048s-2.444-.009-3.298-.048c-2.908-.133-4.52-1.748-4.654-4.654-.039-.853-.048-1.125-.048-3.298 0-2.172.009-2.445.048-3.298.134-2.908 1.748-4.521 4.654-4.653.854-.04 1.125-.049 3.298-.049s2.445.009 3.299.048c2.908.133 4.523 1.751 4.653 4.653.039.854.048 1.127.048 3.299 0 2.173-.009 2.445-.048 3.298z"
                 clip-rule="evenodd"
               />
             </svg>
