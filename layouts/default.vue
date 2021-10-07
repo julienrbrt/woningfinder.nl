5b7cd15d1387edecb13294e0377df77987d80c70
Julien Robert <julien@rbrt.fr>
2021-10-07 12:43:33 +0200

    Update privacy policy and cookie popup (Closes #88)

--- layouts/default.vue
+++ layouts/default.vue
@@ -21,15 +21,6 @@ body {
 }
 </style>
 
-<!-- microsoft clarity -->
-<script type="text/javascript">
-    (function(c,l,a,r,i,t,y){
-        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", "898emfzb68");
-</script>
-
 <!-- facebook pixel -->
 <script>
 !function(f,b,e,v,n,t,s)
